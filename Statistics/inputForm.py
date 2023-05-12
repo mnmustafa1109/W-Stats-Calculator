751ab2b21e6f6686b9f6221e347d51f1f93067d3
syednouman72 <syednouman1618@gmail.com>
2023-05-13 04:49:34 +0500

    setup base and added regex

--- Statistics/inputForm.py
+++ Statistics/inputForm.py
@@ -1,45 +1,51 @@
 from django import forms
 from .setField import setField
+from django.forms import CharField
+import re
 
 class KendallWValueForms(forms.Form):
-    set_1 = setField(label='Set 1', max_length=100, help_text="1,2,3", required=True)
-    set_2 = setField(label='Set 2', max_length=100, help_text="3,4,5", required=True)
+    set_1 = CharField(label='Set 1', max_length=100, help_text="1,2,3", required=True)
+    set_2 = CharField(label='Set 2', max_length=100, help_text="3,4,5", required=True)
 
-    # def clean_set_1(self):
-    #     # raise an exception if the values are not csv
-    #     value = self.cleaned_data.get('set_1')
-    #     # raise forms.ValidationError("VFUCKJKDLFJL")
-    #     err = "someting"
-    #     self.add_error("set_1", err)
-    #     # Check if the value is empty
-    #     # if not value:
-    #     #     raise forms.ValidationError("Set 1 is required.")
-    #
-    #     # Split the value by commas
-    #     # values = value.split(',')
-    #
-    #     # Check if all values are non-empty
-    #     # if not all(values):
-    #     #     raise forms.ValidationError("Values in Set 1 must be comma-separated.")
-    #
-    #     return value
-    #
-    # def clean_set_2(self):
-    #     # raise an exception if the values are not csv
-    #     value = self.cleaned_data.get('set_2')
-    #
-    #     # Check if the value is empty
-    #     if not value:
-    #         raise forms.ValidationError("Set 2 is required.")
-    #
-    #     # Split the value by commas
-    #     values = value.split(',')
-    #
-    #     # Check if all values are non-empty
-    #     if not all(values):
-    #         raise forms.ValidationError("Values in Set 2 must be comma-separated.")
-    #
-    #     return value
+    def clean_set_1(self):
+        # raise an exception if the values are not csv
+        value = self.cleaned_data.get('set_1')
+
+        pattern = re.compile(r"^[0-9\,]*$")
+        if bool(pattern.search(value)) is False:
+            raise forms.ValidationError("Values in Set 1 must be comma-separated.")
+
+        #delete repeated commas
+        value = re.sub(r',+', ',', value)
+        print(value)
+
+        # print(pattern.findall("z")
+        # print(pattern.findall("1,2,3,4"))
+
+        # Check if the value is empty
+        # if not value:
+        #     raise forms.ValidationError("Set 1 is required.")
+        #
+        # # Split the value by commas
+        # values = value.split(',')
+        #
+        # # Check if all values are non-empty
+        # if not all(values):
+        #     raise forms.ValidationError("Values in Set 1 must be comma-separated.")
+
+        return value
+
+    def clean_set_2(self):
+        value = self.cleaned_data.get('set_2')
+
+        pattern = re.compile(r"^[0-9\,]*$")
+        if bool(pattern.search(value)) is False:
+            raise forms.ValidationError("Values in Set 2 must be comma-separated.")
+
+        # delete repeated commas
+        value = re.sub(r',+', ',', value)
+        print(value)
+        return value
 
 
 
--- Statistics/views.py
+++ Statistics/views.py
@@ -4,22 +4,22 @@ from django.forms.models import model_to_dict
 
 
 # Create your views here.
-def take_input(request, error=None):
+def take_input(request, error=""):
     if request.method == 'POST':
         form = KendallWValueForms(request.POST)
         if form.is_valid():
             # print(form.cleaned_data)
             print("Valid")
-            request.session['set_1'] = form['set_1'].value()
-            request.session['set_2'] = form['set_2'].value()
-        else:
-            #print the errors
-            request.session['formerrors'] = form.errors
-            print("Errors: ")
+            request.session['set_1'] = form.cleaned_data['set_1']
+            request.session['set_2'] = form.cleaned_data['set_2']
+            if len(form.cleaned_data['set_1']) != len(form.cleaned_data['set_2']):
+                return render(request, 'index.html', {'form': form, 'error': "The length of two sets must be equal."})
+            return redirect('result')
 
+        else:
+            return render(request, 'index.html', {'form': form, 'error': error})
         print(request.POST)
 
-        return redirect('result' )
     else:
         form = KendallWValueForms()
         context = {
@@ -31,9 +31,7 @@ def take_input(request, error=None):
 
 def display_result(request):
     # print(random)
-    formerros = request.session.get('formerrors')
-    if (formerros):
-        return take_input(request, formerros)
+
     # print(form.errors)
     set_1 = str(request.session.get('set_1'))
-    return render(request, 'result.html', {'result': set_1, 'formerr': formerros})
+    return render(request, 'result.html', {'result': set_1})
